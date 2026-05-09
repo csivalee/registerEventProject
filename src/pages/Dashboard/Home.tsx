45fe60be2a53261250329a8687d7da8941341e98
csivalee <c.sivalee@gmail.com>
2026-05-09 21:17:51 +0700

    update TailAdmin Theme

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,26 +1,32 @@
-import PageBreadcrumb from "../../components/common/PageBreadCrumb";
+import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
+// import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
+// import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
+import RecentOrders from "../../components/ecommerce/RecentOrders";
 import PageMeta from "../../components/common/PageMeta";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <PageMeta
-        title="React.js Blank Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Blank Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
+        description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <PageBreadcrumb pageTitle="Blank Page" />
-      <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12">
-        <div className="mx-auto w-full max-w-[630px] text-center">
-          <h3 className="mb-4 font-semibold text-gray-800 text-theme-xl dark:text-white/90 sm:text-2xl">
-            Card Title Here
-          </h3>
+      <div className="grid grid-cols-12 gap-4 md:gap-6">
+        {/* <div className="col-span-12 space-y-6 xl:col-span-7"> */}
+        <div className="col-span-12 space-y-6 xl:col-span-12">
+          <EcommerceMetrics />
 
-          <p className="text-sm text-gray-500 dark:text-gray-400 sm:text-base">
-            Start putting content on grids or panels, you can also use different
-            combinations of grids.Please check out the dashboard and other pages
-          </p>
+          {/* <MonthlySalesChart /> */}
+        </div>
+
+        <div className="col-span-12 xl:col-span-5">
+          {/* <MonthlyTarget /> */}
+        </div>
+
+        <div className="col-span-12">
+          <RecentOrders />
         </div>
       </div>
-    </div>
+    </>
   );
 }
